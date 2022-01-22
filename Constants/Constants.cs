36c71fb1a1c43a9ebb559f0118258b79bd5bcc72
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-01-22 11:49:42 -0600

    Add project files.

---
+++ Constants/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics.Constants
+{
+    public static class Constants
+    {
+        const double c = 299792458;
+        const double h = 6.62607015e-34;
+        const double hbar = 1.0545718e-34;
+    }
+}
---
+++ Constants/Structure.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics.Constants
+{
+    public class Structure
+    {
+        double Value { get; set; }
+        double Error { get; set; }
+        Dictionary<string, UnitForm>? Units { get; set; }
+
+        public Structure()
+        {
+            this.Units = new Dictionary<string, UnitForm>()
+            {
+                { "Second", 0, 1 },
+                { "Meter", UnitForm.Empty }
+            };
+        }
+    }
+
+    public class UnitName
+    {
+        public string? Second { get; set; }
+        public string? Meter { get; set; }
+        public string? Kilogram { get; set; }
+        public string? Ampere { get; set; }
+        public string? Kelvin { get; set; }
+        public string? Mole { get; set; }
+        public string? Candela { get; set; }
+    }
+
+    public class UnitForm
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public UnitForm(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+    }
+}
