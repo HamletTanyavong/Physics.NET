3377e591eb2d789925273188abe162f5b234a214
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-01-31 01:03:27 -0600

    add some basic mathematical operations
    add methods to compute and estimate factorials and initial code to compute permutations

---
+++ Mathematics/Factorial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics.Mathematics
+{
+    /// <summary>
+    /// Computes the factorial of an integer.
+    /// </summary>
+    public static class Factorial
+    {
+        private static long[] Numerator  = new long[] {1, 1, 1, -139, -571, 163879, 5246819, -534703531, -4483131259, 432261921612371};
+        private static long[] Denominator = new long[] {1, 12, 288, 51840, 2488320, 209018880, 75246796800, 902961561600, 86684309913600, 514904800886784000};
+        
+        /// <summary>
+        /// Computes the factorial of <paramref name="n"/> for <paramref name="n"/> less than or equal to 65. For larger values of <paramref name="n"/>, use approximations.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static ulong General(uint n)
+        {
+            if (n == 0)
+                return 1;
+            ulong result = 1;
+            if (n <= 65)
+            {
+                for (uint i = n; i > 1; i--)
+                {
+                    result *= i;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("n must be less than or equal to 65");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Computes the factorial of <paramref name="n"/> using the Stirling approximation to <paramref name="order"/> no more than 9. Use <paramref name="n"/> less than or equal to 142.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public static double Stirling(uint n, uint order)
+        {
+            double constant = Math.Sqrt(2 * Math.PI * n) * Math.Pow(n, n) / Math.Pow(Math.E, n);
+            double series = 0;
+            for (int i = 0; i <= order; i++)
+            {
+                series += (Numerator[i]) / (Denominator[i] * Math.Pow(n, i));
+            }
+            return constant * series;
+        }
+    }
+}
---
+++ Mathematics/Permutation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics.Mathematics
+{
+    public class Permutation
+    {
+        public uint I { get; set; }
+        public uint J { get; set; }
+        public uint N { get; set; }
+
+        public Permutation(uint i, uint j, uint n)
+        {
+            I = i;
+            J = j;
+            N = n;
+        }
+
+        //public uint Size()
+        //{
+        //    Math.Factor
+        //}
+    }
+}
