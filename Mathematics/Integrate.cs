b309166bd4363378a1b144146a997c206e3f93c6
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-01-31 01:01:34 -0600

    update integration methods
    add sequential integration

--- Mathematics/Integrate.cs
+++ Mathematics/Integrate.cs
@@ -14,40 +14,21 @@ namespace Physics.Mathematics
     /// </summary>
     public static class Integrate
     {
-        /// <summary>
-        /// Integrate a one-dimensional <paramref name="function"/> using the trapezoidal rule.
-        /// </summary>
-        /// <remarks>
-        /// <para>This method utilizes parallel computing to quicken integration but may use more system memory. Efficiency will vary based on different processors.</para>
-        /// </remarks>
-        /// <param name="function">One-dimensional function to be integrated.</param>
-        /// <param name="a">Lower integration bound.</param>
-        /// <param name="b">Upper integration bound.</param>
-        /// <param name="N">N must be an integer greater than zero.</param>
-        /// <exception cref="ResolutionException"></exception>
-        /// <returns>The value of the integral between the bounds <paramref name="a"/> and <paramref name="b"/>.</returns>
         public static double Trapezoidal(Func<double, double> function, double a, double b, int N)
         {
+            var watch = new System.Diagnostics.Stopwatch();
             try
             {
-                if (N < 1)
-                {
-                    throw new ResolutionException($"The number of partitions N must be an integar and greater than zero.", N);
-                }
-
-                var partialSum = new ConcurrentBag<double>();
+                watch.Start();
+                double partialSum = 0;
                 double delta = (b - a) / N;
-                Parallel.For<double>(1, N, () => 0,
-                    (k, state, subtotal) =>
-                    {
-                        return subtotal += function(a + (k * delta));
-                    },
-                    (x) =>
-                    {
-                        partialSum.Add(x);
-                    }
-                );
-                return (partialSum.Sum() + ((function(a) + function(b)) / 2)) * delta;
+                for (int i = 1; i < N; i++)
+                {
+                    partialSum += function(a + (i * delta));
+                }
+                watch.Stop();
+                Console.WriteLine($"Elapsed time for sequential method: {watch.ElapsedMilliseconds} ms");
+                return (partialSum + ((function(a) + function(b)) / 2)) * delta;
             }
             catch (Exception)
             {
@@ -56,7 +37,62 @@ namespace Physics.Mathematics
         }
 
         /// <summary>
-        /// Monte Carlo method of integration for multivariable integrals.
+        /// Integrate a one-dimensional <paramref name="function"/> using the trapezoidal rule.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method utilizes parallel computing to quicken integration but may use more system memory. For a system with 16 processors, the parellel method is quicker than sequential methods for roughly <paramref name="N"/> greater than 250000.</para>
+        /// </remarks>
+        /// <param name="function">One-dimensional function to be integrated.</param>
+        /// <param name="a">Lower integration bound.</param>
+        /// <param name="b">Upper integration bound.</param>
+        /// <param name="N">N must be an integer greater than zero.</param>
+        /// <exception cref="ResolutionException"></exception>
+        /// <returns>The value of the integral between the bounds <paramref name="a"/> and <paramref name="b"/>.</returns>
+        public static double Trapezoidal(Func<double, double> function, double a, double b, uint N, string parallel)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            try
+            {
+                if (N < 1)
+                {
+                    throw new ResolutionException($"The number of partitions N must be an integar and greater than zero.", N);
+                }
+                if (parallel is "CPU")
+                {
+                    watch.Start();
+                    var partialSum = new ConcurrentBag<double>();
+                    double delta = (b - a) / N;
+                    Parallel.For<double>(1, N, () => 0,
+                        (k, loopState, subtotal) =>
+                        {
+                            return subtotal += function(a + (k * delta));
+                        },
+                        (result) =>
+                        {
+                            partialSum.Add(result);
+                        });
+                    watch.Stop();
+                    Console.WriteLine($"Elapsed time for parallel method: {watch.ElapsedMilliseconds} ms");
+                    return (partialSum.Sum() + ((function(a) + function(b)) / 2)) * delta;
+                }
+                else if (parallel is "GPU")
+                {
+                    Console.WriteLine("GPU implementation not ready");
+                    return double.NaN;
+                }
+                else
+                {
+                    throw new ArgumentException("Must specifiy either 'CPU' or 'GPU' for parallelization");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Monte Carlo method of integration for multivariable integrals with importance sampling.
         /// </summary>
         /// <param name="function"></param>
         /// <param name="a"></param>
