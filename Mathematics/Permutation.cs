81cab98bcfc60b3a61b5d75abd1d689eb35b294c
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-05-07 12:14:54 -0500

    reorganize files

--- Mathematics/Permutation.cs
+++ Mathematics/Permutation.cs
@@ -6,22 +6,54 @@ using System.Threading.Tasks;
 
 namespace Physics.Mathematics
 {
+    /// <summary>
+    /// Calculates the permutation of a number of integers.
+    /// </summary>
     public class Permutation
     {
-        public int I { get; set; }
-        public int J { get; set; }
-        public int N { get; set; }
+        private static int N { get; set; }
 
-        public Permutation(int i, int j, int n)
+        public Permutation(int n)
         {
-            I = i;
-            J = j;
             N = n;
         }
 
-        //public int Size()
-        //{
-        //    Math.Factor
-        //}
+        /// <summary>
+        /// Calculates the <paramref name="j"/>th permutation of a sequence.
+        /// </summary>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public int[] Sequence(int j)
+        {
+            int[] result = new int[N];
+            for (int l = 1; l <= N; l++)
+            {
+                var sum = 0;
+                for (int k = 0; k < l - 1; k++)
+                {
+                    if (Cycle(j, l) >= Cycle(j, k + 1))
+                    {
+                        sum += 1;
+                    }
+                    else
+                    {
+                        ;
+                    }
+                }
+                result[l - 1] = Cycle(j, l) + sum;
+                //Console.WriteLine(sum);
+            }
+            return result;
+        }
+
+        public int Inverse(int[] sequence)
+        {
+            return int.MaxValue;
+        }
+
+        private static int Cycle(int j, int l)
+        {
+            return 1 + ((j - 1) / General.Factorial(N - l)) % (N - l + 1);
+        }
     }
 }
---
+++ Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Physics
+{
+    public static class Session
+    {
+        internal static int Signature { get; set; }
+        internal static string? Units { get; set; }
+
+        public static void Initialize(string units)
+        {
+            SetUnits(units);
+        }
+
+        private static void SetUnits(string units)
+        {
+            Units = units;
+        }
+    }
+}
