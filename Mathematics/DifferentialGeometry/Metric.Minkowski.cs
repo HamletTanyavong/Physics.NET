b82473ed9257fd4dc8e5b64ee1c6627a26f038ff
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-05-28 12:33:59 -0500

    let metric use cartesian, cylindrical, and spherical spacial coordinates

--- Mathematics/DifferentialGeometry/Metric.Minkowski.cs
+++ Mathematics/DifferentialGeometry/Metric.Minkowski.cs
@@ -2,24 +2,46 @@
 {
     public static partial class Metric
     {
+        private readonly static int sc = Session.SigConst;
+
         /// <summary>
-        /// Calculate the value of the Minkowski metric at a point in spacetime represented by <paramref name="fourVector"/>.
+        /// Calculate the value of the Minkowski metric at a point in spacetime represented by <paramref name="fourvector"/>. If <paramref name="inverse"/> is true, then the inverse metric will be used for the calculation.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="fourVector"></param>
+        /// <param name="fourvector"></param>
         /// <returns></returns>
-        public static FourVector<T> Minkowski<T>(FourVector<T> fourVector)
+        public static FourVector<T> Minkowski<T>(bool inverse, FourVector<T> fourvector)
             where T : class, ICoordinateSystem, I3D
         {
-            if (Session.Signature is "Spacelike")
+            string coordinateSystem = typeof(T).Name;
+
+            if (coordinateSystem is "Cartesian")
             {
-                FourVector<T> result = new(-1 * fourVector.Zeroth, fourVector.First, fourVector.Second, fourVector.Third);
-                return result;
+                return new FourVector<T>(-1 * sc, sc, sc, sc);
+            }
+            else if (coordinateSystem is "Cylindrical")
+            {
+                if (inverse is false)
+                {
+                    return new FourVector<T>(-1 * sc, sc, fourvector.First * fourvector.First * sc, sc);
+                }
+                else
+                {
+                    return new FourVector<T>(-1 * sc, sc, sc / (fourvector.First * fourvector.First), sc);
+                }
             }
             else
             {
-                FourVector<T> result = new(fourVector.Zeroth, -1 * fourVector.First, -1 * fourVector.Second, -1 * fourVector.Third);
-                return result;
+                var firstSquared = fourvector.First * fourvector.First;
+                var sinSquared = Math.Sin(fourvector.Second) * Math.Sin(fourvector.Second);
+                if (inverse is false)
+                {
+                    return new FourVector<T>(-1 * sc, sc, firstSquared * sc, firstSquared * sinSquared * sc);
+                }
+                else
+                {
+                    return new FourVector<T>(-1 * sc, sc, sc / firstSquared, sc / (firstSquared * sinSquared));
+                }
             }
         }
     }
