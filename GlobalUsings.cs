17f2797278d346252422380b9e298abf40e40d72
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-02 10:20:13 -0500

    Update global usings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,5 @@
-﻿global using Physics.NET.Mathematics;
+﻿global using Physics.NET.Exceptions;
+global using Physics.NET.Mathematics;
+global using Physics.NET.Mathematics.Helper;
 global using Physics.NET.Mathematics.Vectors;
 global using Physics.NET.Units;
