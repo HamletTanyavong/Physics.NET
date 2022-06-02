5deb99f5389071a135e9f11f0de129cc801fdb95
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-02 10:11:46 -0500

    Add helper class to aid with picking coordinates and operations

---
+++ Mathematics/Helper/Selector.Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using Physics.NET.Mathematics.DifferentialGeometry;
+
+namespace Physics.NET.Mathematics.Helper
+{
+    internal static partial class Selector
+    {
+        internal static FourVector<T, I> Coordinate<T, I>(OperationMethod<T, I> method, string operation, FourVector<T, I> a, FourVector<T, I> b)
+            where T : class, ICoordinateSystem, I3D
+            where I : class, IIndexPosition
+        {
+            var coordinateSystem = typeof(T).Name;
+            return typeof(T).Name switch
+            {
+                "Cartesian" => method("Cartesian", operation, a, b),
+                "Cylindrical" => method("Cylindrical", operation, a, b),
+                "Spherical" => method("Spherical", operation, a, b),
+                _ => throw new TypeAccessException($"{coordinateSystem} is not a valid coordinate system"),
+            };
+        }
+
+        internal delegate FourVector<T, I> OperationMethod<T, I>(string coordinate, string operation, FourVector<T, I> a, FourVector<T, I> b)
+            where T : class, ICoordinateSystem, I3D
+            where I : class, IIndexPosition;
+    }
+}
