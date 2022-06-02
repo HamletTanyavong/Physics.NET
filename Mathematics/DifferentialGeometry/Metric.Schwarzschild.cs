6345f4142a68f0bd597b9db02f6517369db9aaef
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-02 14:05:42 -0500

    Update Metric.Schwarzschild.cs

--- Mathematics/DifferentialGeometry/Metric.Schwarzschild.cs
+++ Mathematics/DifferentialGeometry/Metric.Schwarzschild.cs
@@ -3,13 +3,14 @@
     public static partial class Metric
     {
         /// <summary>
-        /// Calculate the value of the Schwarzschild metric for an object of mass <paramref name="M"/> at a point in spacetime represented by <paramref name="fourvector"/>. If <paramref name="inverse"/> is true, then the inverse metric will be used for the calculation. Coordinates must be spherical.
+        /// Calculate the value of the Schwarzschild metric for an object of mass <paramref name="M"/> at a point in spacetime represented by a <paramref name="fourvector"/>. Takes in a four-vector with a contravariant index. Coordinates must be spherical.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="inverse"></param>
         /// <param name="M"></param>
         /// <param name="fourvector"></param>
         /// <returns></returns>
+        /// <exception cref="TypeAccessException"></exception>
         public static FourVector<T, L> Schwarzschild<T>(double M, FourVector<T, U> fourvector)
             where T : class, ICoordinateSystem, I3D
         {
@@ -31,13 +32,13 @@
         }
 
         /// <summary>
-        /// Calculate the value of the Schwarzschild metric for an object of mass <paramref name="M"/> at a point in spacetime represented by <paramref name="fourvector"/>. If <paramref name="inverse"/> is true, then the inverse metric will be used for the calculation. Coordinates must be spherical.
+        /// Calculate the value of the inverse Schwarzschild metric for an object of mass <paramref name="M"/> at a point in spacetime represented by a <paramref name="fourvector"/>. Takes in a four-vector with a covariant index. Coordinates must be spherical.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="inverse"></param>
         /// <param name="M"></param>
         /// <param name="fourvector"></param>
         /// <returns></returns>
+        /// <exception cref="TypeAccessException"></exception>
         public static FourVector<T, U> Schwarzschild<T>(double M, FourVector<T, L> fourvector)
             where T : class, ICoordinateSystem, I3D
         {
