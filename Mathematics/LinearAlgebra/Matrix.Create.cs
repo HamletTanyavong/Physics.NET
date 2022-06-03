a0650ee83cb4b99d28e47b824f6c5e1729f6d4cc
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-03 06:47:39 -0500

    Move matrix file to linear algebra folder

---
+++ Mathematics/LinearAlgebra/Matrix.Create.cs
@@ -0,0 +1,16 @@
+﻿namespace Physics.NET.Mathematics.LinearAlgebra
+{
+    public static partial class Matrix
+    {
+        public static double[,] Diagonal(double zeroth, double first, double second, double third)
+        {
+            return new double[,]
+            {
+                { zeroth, 0,     0,      0     },
+                { 0,      first, 0,      0     },
+                { 0,      0,     second, 0     },
+                { 0,      0,     0,      third }
+            };
+        }
+    }
+}
