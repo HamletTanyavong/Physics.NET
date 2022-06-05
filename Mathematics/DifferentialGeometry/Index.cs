5054852b71a6a499af575cf2aea2611da50e0a2e
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-05 01:16:00 -0500

    Remove unused constructor

--- Mathematics/DifferentialGeometry/Index.cs
+++ Mathematics/DifferentialGeometry/Index.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Physics.NET.Mathematics.DifferentialGeometry
+﻿namespace Physics.NET.Mathematics.DifferentialGeometry
 {
     /// <summary>
     /// Represents a tensor index.
     /// </summary>
-    public struct Index<T> where T : class, IIndexPosition
+    public struct Index<T> : IIndex where T : class, IIndexPosition
     {
         public int? Location { get; set; } = null;
         public string IndexName { get; set; } = string.Empty;
@@ -18,12 +12,6 @@ namespace Physics.NET.Mathematics.DifferentialGeometry
         {
             Location = null;
             IndexName = String.Empty;
-        }
-
-        public Index(int location, string index)
-        {
-            Location = location;
-            IndexName = index;
         }
     }
 }
