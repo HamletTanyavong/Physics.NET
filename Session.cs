a0c18a7859a7095a629aea7479ed1366eb237473
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-06 12:02:47 -0500

    Remove usings

--- Session.cs
+++ Session.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Physics.NET.Units;
-using Physics.NET.Mathematics.DifferentialGeometry;
+﻿using Physics.NET.Mathematics.DifferentialGeometry;
 
 namespace Physics.NET
 {
