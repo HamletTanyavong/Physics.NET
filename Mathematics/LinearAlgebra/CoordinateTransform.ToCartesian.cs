d0de94102a63c57560f9bf4f03cdcbcb667570ac
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-07 18:55:08 -0500

    Update CoordinateTransform.ToCartesian.cs

--- Mathematics/LinearAlgebra/CoordinateTransform.ToCartesian.cs
+++ Mathematics/LinearAlgebra/CoordinateTransform.ToCartesian.cs
@@ -6,12 +6,12 @@ namespace Physics.NET.Mathematics.LinearAlgebra
     {
         public static Vector3D<Cartesian> ToCartesian(Vector3D<Cylindrical> a)
         {
-            return new Vector3D<Cartesian>(a.First * Math.Cos(a.Second), a.First * Math.Sin(a.Second), a.Third);
+            return new Vector3D<Cartesian>(a.X1 * Math.Cos(a.X2), a.X1 * Math.Sin(a.X2), a.X3);
         }
 
         public static Vector3D<Cartesian> ToCartesian(Vector3D<Spherical> a)
         {
-            return new Vector3D<Cartesian>(a.First * Math.Cos(a.Third) * Math.Sin(a.Second), a.First * Math.Sin(a.Third) * Math.Sin(a.Second), a.First * Math.Cos(a.Second));
+            return new Vector3D<Cartesian>(a.X1 * Math.Cos(a.X3) * Math.Sin(a.X2), a.X1 * Math.Sin(a.X3) * Math.Sin(a.X2), a.X1 * Math.Cos(a.X2));
         }
     }
 }
