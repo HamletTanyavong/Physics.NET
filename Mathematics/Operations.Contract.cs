fc4335ccc088f761ebe7db862f9865b898d82b09
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-07 18:55:43 -0500

    Update Operations.Contract.cs

--- Mathematics/Operations.Contract.cs
+++ Mathematics/Operations.Contract.cs
@@ -7,13 +7,21 @@ namespace Physics.NET.Mathematics
         public static double Contract<T>(FourVector<T, U> a, FourVector<T, L> b)
             where T : class, ICoordinateSystem, I3D
         {
-            return a.Zeroth * b.Zeroth + a.First * b.First + a.Second * b.Second + a.Third * b.Third;
+            if (a._Index.IndexName != b._Index.IndexName)
+            {
+                throw new IndexMismatchException($"error: Indicies do not match");
+            }
+            return a.X0 * b.X0 + a.X1 * b.X1 + a.X2 * b.X2 + a.X3 * b.X3;
         }
 
         public static double Contract<T>(FourVector<T, L> a, FourVector<T, U> b)
             where T : class, ICoordinateSystem, I3D
         {
-            return a.Zeroth * b.Zeroth + a.First * b.First + a.Second * b.Second + a.Third * b.Third;
+            if (a._Index.IndexName != b._Index.IndexName)
+            {
+                throw new IndexMismatchException($"error: Indicies do not match");
+            }
+            return a.X0 * b.X0 + a.X1 * b.X1 + a.X2 * b.X2 + a.X3 * b.X3;
         }
     }
 }
