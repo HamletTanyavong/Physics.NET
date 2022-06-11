8a00afa71230336997cd133021fe3188b4e8353c
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-11 04:06:28 -0500

    Add complex Log method

---
+++ Mathematics/Op.Log.cs
@@ -0,0 +1,15 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        /// <summary>
+        /// Calculates the principle value natural log of a complex number <paramref name="z"/>.
+        /// </summary>
+        /// <param name="z"></param>
+        /// <returns>Returns the result in polar form.</returns>
+        public static Complex Ln(Complex z)
+        {
+            return new(Math.Log(z.Magnitude), z.Phase);
+        }
+    }
+}
