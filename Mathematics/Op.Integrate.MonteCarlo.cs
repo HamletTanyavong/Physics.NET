346d3fd4cd929f4e82f8d14126141ecba97eeee9
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-06-11 04:01:43 -0500

    Add crude Monte Carlo integration method for functions of two variables

---
+++ Mathematics/Op.Integrate.MonteCarlo.cs
@@ -0,0 +1,71 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        public static double Integrate(Func<double, double, bool> domain, Func<double, double, double> function, ValueTuple<string, double, double> rangeX, ValueTuple<string, double, double> rangeY, int N)
+        {
+            Random random = new();
+
+            if (rangeX.Item2 > rangeX.Item3 || rangeY.Item2 > rangeY.Item3)
+            {
+                throw new ArgumentException("error: invalid bounds");
+            }
+
+            double mx = rangeX.Item3 - rangeX.Item2;
+            double bx = rangeX.Item2;
+            double my = rangeY.Item3 - rangeY.Item2;
+            double by = rangeY.Item2;
+
+            double Area = rangeX.Item3 - rangeX.Item2 * rangeY.Item3 - rangeY.Item2;
+            double partialSum = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                double x = random.NextDouble() * mx + bx;
+                double y = random.NextDouble() * my + by;
+
+                if (domain(x, y))
+                {
+                    partialSum += function(x, y);
+                }
+            }
+            return partialSum * Area / N;
+        }
+
+        public static double Integrate(Func<Vector2D<Cartesian>, bool> domain, Func<Vector2D<Cartesian>, double> function, ValueTuple<string, double, double> rangeX, ValueTuple<string, double, double> rangeY, int N)
+        {
+            Random random = new();
+
+            if (rangeX.Item2 > rangeX.Item3 || rangeY.Item2 > rangeY.Item3)
+            {
+                throw new ArgumentException("error: invalid bounds");
+            }
+
+            double mx = rangeX.Item3 - rangeX.Item2;
+            double bx = rangeX.Item2;
+            double my = rangeY.Item3 - rangeY.Item2;
+            double by = rangeY.Item2;
+
+            double Area = rangeX.Item3 - rangeX.Item2 * rangeY.Item3 - rangeY.Item2;
+            double partialSum = 0;
+
+            Vector2D<Cartesian> input = new();
+
+            for (int i = 0; i < N; i++)
+            {
+                double x = random.NextDouble() * mx + bx;
+                double y = random.NextDouble() * my + by;
+
+                input.X1 = x;
+                input.X2 = y;
+
+                //Domain.Sample(random, domain, rangeX, rangeY);
+                if (domain(input))
+                {
+                    partialSum += function(input);
+                }
+            }
+            return partialSum * Area / N;
+        }
+    }
+}
