2e76280ede86e1fdcdf3a2eb4dab6be975c55b73
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-08 08:10:33 -0500

    Move factorial method to new class

--- Mathematics/Op.Factorial.cs
+++ Mathematics/Op.Factorial.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Physics.NET.Mathematics
+﻿namespace Physics.NET.Mathematics
 {
     /// <summary>
     /// General Mathematical Functions.
     /// </summary>
-    public static class General
+    public static partial class Op
     {
-        private static readonly long[] Numerator  = new long[] {1, 1, 1, -139, -571, 163879, 5246819, -534703531, -4483131259, 432261921612371};
-        private static readonly long[] Denominator = new long[] {1, 12, 288, 51840, 2488320, 209018880, 75246796800, 902961561600, 86684309913600, 514904800886784000};
-        
         /// <summary>
         /// Computes the factorial of <paramref name="n"/> for <paramref name="n"/> less than or equal to 65. For larger values of <paramref name="n"/>, use approximations.
         /// </summary>
@@ -43,9 +34,17 @@ namespace Physics.NET.Mathematics
         /// </summary>
         /// <param name="n"></param>
         /// <param name="order"></param>
-        /// <returns></returns>
+        /// <returns>Returns an approximation of the factorial of <paramref name="n"/>.</returns>
         public static double Stirling(int n, int order)
         {
+            if (order > 9)
+            {
+                throw new ArgumentException("error: order too high");
+            }
+
+            long[] Numerator = new long[] { 1, 1, 1, -139, -571, 163879, 5246819, -534703531, -4483131259, 432261921612371 };
+            long[] Denominator = new long[] { 1, 12, 288, 51840, 2488320, 209018880, 75246796800, 902961561600, 86684309913600, 514904800886784000 };
+
             double constant = Math.Sqrt(2 * Math.PI * n) * Math.Pow(n, n) / Math.Pow(Math.E, n);
             double series = 0;
             for (int i = 0; i <= order; i++)
