231a24babc7358afb9b48c7700f3dd9e182fbcd0
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-10 13:58:50 -0500

    Create Ut.cs

---
+++ Utility/Ut.cs
@@ -0,0 +1,17 @@
+﻿namespace Physics.NET.Utility
+{
+    public static partial class Ut
+    {
+        public static string ToString(int[] array)
+        {
+            string result = "{";
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += $"{array[i]},";
+            }
+            result = result[..^1];
+            result += "}";
+            return result;
+        }
+    }
+}
