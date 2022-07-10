1e27ab57449f72bb3555543bda275abb4ea4da1b
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-10 13:53:48 -0500

    Update ITensor.cs

--- Mathematics/DifferentialGeometry/ITensor.cs
+++ Mathematics/DifferentialGeometry/ITensor.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Physics.NET.Mathematics.DifferentialGeometry
+﻿namespace Physics.NET.Mathematics.DifferentialGeometry
 {
     public interface ITensor
     {
         /// <summary>
         /// The rank of a tensor.
         /// </summary>
-        int Rank { get; }
+        static int Rank;
 
         string GetIndex();
         string ToString();
