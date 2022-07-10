14dc57bd2ba3ba791b2fb07acdb2da8eef4d77d0
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-10 13:58:37 -0500

    Create Domain.Linspace.cs

---
+++ Mathematics/Domain.Linspace.cs
@@ -0,0 +1,17 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Domain
+    {
+        public static double[] Linspace(double start, double stop, int n, bool endpoint=true)
+        {
+            double[] result = endpoint ? new double[n] : new double[n - 1];
+            int points = endpoint ? n : n - 1;
+            double delta = (stop - start) / n;
+            for (int i = 0; i < points; i++)
+            {
+                result[i] = start + (i * delta);
+            }
+            return result;
+        }
+    }
+}
