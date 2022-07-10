f27ab70d86aedec1f30fa3704df35d6252291266
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-10 13:53:28 -0500

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,5 +2,6 @@
 global using Physics.NET.Mathematics;
 global using Physics.NET.Mathematics.LinearAlgebra;
 global using Physics.NET.Units;
+global using Physics.NET.Utility;
 
 global using System.Numerics;
