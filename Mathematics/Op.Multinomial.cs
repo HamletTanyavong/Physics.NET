ceec852e40afa84de2aef0a87824133d88485e8c
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-11 23:01:32 -0500

    Add binomial and multinomial coefficients

---
+++ Mathematics/Op.Multinomial.cs
@@ -0,0 +1,33 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        /// <summary>
+        /// The binomial coefficient.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <returns><paramref name="n"/> choose <paramref name="k"/></returns>
+        public static double Binomial(int n, int k)
+        {
+            return Op.Factorial(n) / (Op.Factorial(k) * Op.Factorial(n - k));
+        }
+
+        /// <summary>
+        /// The multinomial coefficient.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="α"></param>
+        /// <returns><paramref name="n"/> choose (<paramref name="α"/>₁ . . . <paramref name="α"/>ₛ)</returns>
+        public static double Multinomial(int n, int[] α)
+        {
+            int order = α.Length;
+            double partialResult = Op.Factorial(n);
+            for (int i = 0; i < order; i++)
+            {
+                partialResult /= Op.Factorial(α[i]);
+            }
+            return partialResult;
+        }
+    }
+}
