82acf1483df40af8d42c4a3ca8f1a11e0207cfbe
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-13 01:01:47 -0500

    Create Op.Im.cs
    Add complex unit

---
+++ Mathematics/Op.Im.cs
@@ -0,0 +1,13 @@
+﻿namespace Physics.NET.Mathematics
+{
+    /// <summary>
+    /// General mathematical functions.
+    /// </summary>
+    public static partial class Op
+    {
+        /// <summary>
+        /// Complex unit.
+        /// </summary>
+        public static readonly Complex Im = new(0, 1);
+    }
+}
