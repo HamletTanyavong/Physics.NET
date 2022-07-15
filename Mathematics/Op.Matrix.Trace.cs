9839b7e7a6484bb79544d65b98e6f04f8114df75
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-15 00:56:58 -0500

    Create Op.Matrix.Trace.cs

---
+++ Mathematics/Op.Matrix.Trace.cs
@@ -0,0 +1,23 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        public static partial class Matrix
+        {
+            public static double Trace(double[,] matrix)
+            {
+                if (!IsSquare(matrix))
+                {
+                    throw new ArgumentException("error: matrix is not square");
+                }
+
+                double partialSum = 0;
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    partialSum += matrix[i, i];
+                }
+                return partialSum;
+            }
+        }
+    }
+}
