866a5ff8e0290dd8016497c1943e5bc78fd9c6c3
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-15 01:00:51 -0500

    Create Op.Exp.cs

---
+++ Mathematics/Op.Exp.cs
@@ -0,0 +1,10 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        public static Complex Exp(Complex z)
+        {
+            return Complex.Exp(z);
+        }
+    }
+}
