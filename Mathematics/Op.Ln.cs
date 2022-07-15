659c5ccd35b6c45704317e8696cb3c028fb29b16
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-15 01:01:12 -0500

    Update Op.Ln.cs

--- Mathematics/Op.Ln.cs
+++ Mathematics/Op.Ln.cs
@@ -6,7 +6,7 @@
         /// Calculates the principle value natural log of a complex number <paramref name="z"/>.
         /// </summary>
         /// <param name="z"></param>
-        /// <returns>Returns the result in polar form.</returns>
+        /// <returns>The natural log of <paramref name="z"/>.</returns>
         public static Complex Ln(Complex z)
         {
             return Complex.Log(z);
