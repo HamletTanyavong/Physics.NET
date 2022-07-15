943cf5487900504a426bffec0525400693452066
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-15 01:00:42 -0500

    Update TensorR2.cs

--- Mathematics/DifferentialGeometry/TensorR2.cs
+++ Mathematics/DifferentialGeometry/TensorR2.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Physics.NET.Mathematics.DifferentialGeometry
+﻿namespace Physics.NET.Mathematics.DifferentialGeometry
 {
     public struct TensorR2
     {
