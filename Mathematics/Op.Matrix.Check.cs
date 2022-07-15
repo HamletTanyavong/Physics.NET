eb2cc96b148a18b2cc64717ae602f81641873b58
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-15 00:38:40 -0500

    Create Op.Matrix.Check.cs
    Create method to check if an array is square

---
+++ Mathematics/Op.Matrix.Check.cs
@@ -0,0 +1,13 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        public static partial class Matrix
+        {
+            public static bool IsSquare<T>(T[,] matrix)
+            {
+                return matrix.GetLength(0) == matrix.GetLength(1);
+            }
+        }
+    }
+}
