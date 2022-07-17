869be36b7be51911a6ff50eb7de3a4cbdc601d6b
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-17 04:20:39 -0500

    Add Kronecker delta.

---
+++ Mathematics/Op.Delta.cs
@@ -0,0 +1,16 @@
+﻿namespace Physics.NET.Mathematics
+{
+    public static partial class Op
+    {
+        /// <summary>
+        /// Kronecker delta with indicies <paramref name="i"/> and <paramref name="j"/>.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns>1 if <paramref name="i"/> = <paramref name="j"/>. Otherwise returns 0.</returns>
+        public static int Delta(int i, int j)
+        {
+            return i != j ? 0 : 1;
+        }
+    }
+}
