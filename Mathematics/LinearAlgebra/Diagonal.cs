e1ca2a4b4530dfc5dd93029f1fa6947d10dea939
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-24 01:13:04 -0500

    Move file

---
+++ Mathematics/LinearAlgebra/Diagonal.cs
@@ -0,0 +1,19 @@
+﻿namespace Physics.NET.Mathematics.LinearAlgebra
+{
+    /// <summary>
+    /// Instantiate a diagonal matrix.
+    /// </summary>
+    public static class Diagonal
+    {
+        /// <summary>
+        /// Instatiate a 2 x 2 diagonal matrix.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>A 2 x 2 matrix with elements <paramref name="a"/> and <paramref name="b"/> along the diagonal.</returns>
+        public static MatSq2 MatSq2(Complex a, Complex b)
+        {
+            return new(new Complex[,] { { a, 0 }, { 0, b } });
+        }
+    }
+}
