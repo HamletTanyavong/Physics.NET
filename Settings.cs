b45d06304b3e9bc1ff2b9d4ca771e5941b1d9b9e
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-24 01:00:48 -0500

    Create Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,45 @@
+﻿namespace Physics.NET
+{
+    public enum Units
+    {
+        /// <summary>
+        /// International System of Units
+        /// </summary>
+        SI,
+
+        /// <summary>
+        /// Natural Units
+        /// </summary>
+        Natural,
+
+        /// <summary>
+        /// CGS Units
+        /// </summary>
+        CGS
+    }
+
+    /// <summary>
+    /// Metric signature.
+    /// </summary>
+    public enum Signature
+    {
+        /// <summary>
+        /// Spacelike: (-1, 1, 1, 1)
+        /// </summary>
+        Spacelike,
+
+        /// <summary>
+        /// Timelike: (1, -1, -1, -1)
+        /// </summary>
+        Timelike
+    }
+
+    /// <summary>
+    /// Align elements in output.
+    /// </summary>
+    public enum Align
+    {
+        Left,
+        Right
+    }
+}
