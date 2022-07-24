60309db39e6abe1b49069b1b09222f06346e0f99
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-24 01:56:27 -0500

    Update Extensions.Output.cs

--- Utility/Extensions.Output.cs
+++ Utility/Extensions.Output.cs
@@ -9,10 +9,10 @@
         /// <returns></returns>
         public static string Output<T>(this T[] array)
         {
-            string result = "{ ";
+            string result = "[";
             result += String.Join(", ", array);
             result = result[..^1];
-            return result += " }";
+            return result += "]";
         }
 
         /// <summary>
@@ -22,24 +22,16 @@
         /// <returns></returns>
         public static string Output(this Complex[] array)
         {
-            string result = "{";
+            string result = "[";
+            string[] hold = new string[array.Length];
             for (int i = 0; i < array.Length; i++)
             {
-                string sign;
-                string im = array[i].Imaginary.ToString();
-                if (array[i].Imaginary >= 0)
-                {
-                    sign = "+";
-                }
-                else
-                {
-                    sign = "-";
-                    im = im[1..];
-                }
-                result += $"{array[i].Real}{sign}i{im},";
+                double rePart = array[i].Real;
+                double imPart = array[i].Imaginary;
+                hold[i] = ComplexToString(rePart, imPart);
             }
-            result = result[..^1];
-            return result += "}";
+            result += String.Join(", ", hold);
+            return result += "]";
         }
 
         public static string Output<T>(this T[,] array) where T : struct, IEquatable<T>, IFormattable
@@ -47,7 +39,7 @@
             int m = array.GetLength(0);
             int n = array.GetLength(1);
 
-            int[] rowLengths = GetMaxRowLengths(array, m, n);
+            int maxRowLength = GetMaxRowLength(array, m, n);
             string[] hold = new string[n];
 
             string result = String.Empty;
@@ -55,22 +47,72 @@
             {
                 for (int j = 0; j < n; j++)
                 {
-                    hold[j] = array[i, j]!.ToString()!.PadLeft(rowLengths[j]);
+                    hold[j] = array[i, j]!.ToString()!.PadRight(maxRowLength);
                 }
+                hold[0] = (i != 0 ? " [" : "[[") + hold[0];
+                hold[n - 1] += i != m - 1 ? "]," : "]]";
+                result += String.Join(" ", hold);
+                result += $"\n";
+            }
+            return result[..^1];
+
+            static int GetMaxRowLength<S>(S[,] array, int m, int n)
+            {
+                int result = 1;
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        var length = array[i, j]!.ToString()!.Length;
+                        if (result < length)
+                        {
+                            result = length;
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Formats a <paramref name="matrix"/> into a neat string.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>Formatted string of <paramref name="matrix"/> values.</returns>
+        public static string Output(this IMatrix matrix)
+        {
+            int m = matrix.M;
+            int n = matrix.N;
+
+            int[] rowLengths = GetMaxRowLengths(matrix, m, n);
+            string[] hold = new string[n];
+
+            string result = String.Empty;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var z = matrix[i, j];
+                    hold[j] = ComplexToString(z.Real, z.Imaginary).PadLeft(rowLengths[j]);
+                }
+                hold[0] = (i != 0 ? " [" : "[[") + hold[0];
+                hold[n - 1] += i != m - 1 ? "]," : "]]";
                 result += String.Join(", ", hold);
                 result += $"\n";
             }
             return result[..^1];
 
-            static int[] GetMaxRowLengths<S>(S[,] array, int m, int n)
+            static int[] GetMaxRowLengths(IMatrix array, int m, int n)
             {
                 int[] result = new int[n];
                 for (int i = 0; i < n; i++)
                 {
-                    int maxLength = array[0, i]!.ToString()!.Length;
+                    var startNum = array[0, i];
+                    int maxLength = ComplexToString(startNum.Real, startNum.Imaginary).Length;
                     for (int j = 1; j < m; j++)
                     {
-                        int length = array[j, i]!.ToString()!.Length;
+                        var iterNum = array[j, i];
+                        int length = ComplexToString(iterNum.Real, iterNum.Imaginary).Length;
                         if (maxLength < length)
                         {
                             maxLength = length;
