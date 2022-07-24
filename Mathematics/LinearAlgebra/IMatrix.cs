5e8fcc74e08d43077e1976ec9ee49ec9ad27b142
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2022-07-24 01:53:49 -0500

    Create IMatrix.cs

---
+++ Mathematics/LinearAlgebra/IMatrix.cs
@@ -0,0 +1,17 @@
+﻿namespace Physics.NET.Mathematics.LinearAlgebra
+{
+    public interface IMatrix
+    {
+        /// <summary>
+        /// Number of rows in a matrix.
+        /// </summary>
+        int M { get; }
+
+        /// <summary>
+        /// Number of Columns in a matrix.
+        /// </summary>
+        int N { get; }
+
+        Complex this[int m, int n] { get; set; }
+    }
+}
