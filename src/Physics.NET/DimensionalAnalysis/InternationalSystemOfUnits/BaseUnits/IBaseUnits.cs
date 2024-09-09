4efcb0c79af8b2c935877375685f18d9d015f4ed
Hamlet Tanyavong <34531738+HamletTanyavong@users.noreply.github.com>
2024-09-08 22:06:13 -0500

    Create IBaseUnits.cs
    Add an interface that defines the SI base units.

---
+++ src/Physics.NET/DimensionalAnalysis/InternationalSystemOfUnits/BaseUnits/IBaseUnits.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="IBaseUnits.cs" company="Physics.NET">
+// Physics.NET
+// https://github.com/HamletTanyavong/Physics.NET
+//
+// MIT License
+//
+// Copyright (c) 2024-present Hamlet Tanyavong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// </copyright>
+
+namespace Physics.NET.DimensionalAnalysis.InternationalSystemOfUnits.BaseUnits;
+
+/// <summary>Represents the SI base units.</summary>
+/// <typeparam name="TBaseUnits">The type that implements the interface.</typeparam>
+/// <typeparam name="TTime">The type that represents the unit of time.</typeparam>
+/// <typeparam name="TLength">The type that represents the unit of length.</typeparam>
+/// <typeparam name="TMass">The type that represents the unit of mass.</typeparam>
+/// <typeparam name="TCurrent">The type that represents the unit of electric current.</typeparam>
+/// <typeparam name="TTemperature">The type that represents the unit of thermodynamic temperature.</typeparam>
+/// <typeparam name="TAmount">The type that represents the unit of an amount of substance.</typeparam>
+/// <typeparam name="TIntensity">The type that represents the unit of luminous intensity.</typeparam>
+internal interface IBaseUnits<TBaseUnits, TTime, TLength, TMass, TCurrent, TTemperature, TAmount, TIntensity>
+    where TBaseUnits : IBaseUnits<TBaseUnits, TTime, TLength, TMass, TCurrent, TTemperature, TAmount, TIntensity>
+    where TTime : ITime<TTime>
+    where TLength : ILength<TLength>
+    where TMass : IMass<TMass>
+    where TCurrent : ICurrent<TCurrent>
+    where TTemperature : ITemperature<TTemperature>
+    where TAmount : IAmount<TAmount>
+    where TIntensity : IIntensity<TIntensity>
+{
+    /// <summary>Represents the SI base quantity of time.</summary>
+    TTime Time { get; init; }
+
+    /// <summary>Represents the SI base quantity of length.</summary>
+    TLength Length { get; init; }
+
+    /// <summary>Represents the SI base quantity of mass.</summary>
+    TMass Mass { get; init; }
+
+    /// <summary>Represents the SI base quantity of electric current.</summary>
+    TCurrent Current { get; init; }
+
+    /// <summary>Represents the SI base quantity of thermodynamic temperature.</summary>
+    TTemperature Temperature { get; init; }
+
+    /// <summary>Represents the SI base quantity of an amount of substance.</summary>
+    TAmount Amount { get; init; }
+
+    /// <summary>Represents the SI base quantity of luminous intensity.</summary>
+    TIntensity Intensity { get; init; }
+}
